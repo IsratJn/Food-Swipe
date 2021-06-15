87d6d0d767557aee04ffb54332b22a0d2496aa7c
Israt33 <42838747+IsratJn@users.noreply.github.com>
2021-06-15 21:13:32 +0600

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,17 @@
+// make database first, model
+// create controller,
+// create route
+
+const CoffeeSchema = new Schema({ name: String, employee: [{ type: Schema.Types.ObjectId, ref: 'Employee' }] });
+
+const AuthSchema = new Schema({
+  name: String,
+  role: { type: String, enum: ['Customer', 'CoffeeShop'],
+  default: 'Customer' },
+  coffeShop: { type: Sceham}
+}); 
+
+if (user.role === 'CoffeeShop')
+//componentWillMount() 
+{ this.props.fetchCoffeeApi(this.props.user.id) };
+
